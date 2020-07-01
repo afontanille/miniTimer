1a5cd0df3fda23c18f9592cc2fa145b2ac2d1af6
Antonin Fontanille <afontanille@users.noreply.github.com>
2020-07-01 19:15:03 +0200

    Initial Commit

---
+++ miniTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  miniTimer
+//
+//  Created by Antonin Fontanille on 01/07/2020.
+//  Copyright © 2020 Antonin Fontanille. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ miniTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  miniTimer
+//
+//  Created by Antonin Fontanille on 01/07/2020.
+//  Copyright © 2020 Antonin Fontanille. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
